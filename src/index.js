ebcf5a13c103575243098a7876d8d568ac7b5727
kesel <a.kiselyoff@gmail.com>
2022-04-18 01:37:11 +0300

    markup fn add

--- src/index.js
+++ src/index.js
@@ -1,6 +1,51 @@
 import './sass/main.scss';
-
+import axios from 'axios';
+import markup from './js/markup';
 import { Notify } from 'notiflix';
 
-Notify.failure('oops');
-console.log('oooooo');
+// console.log('oooooo');
+// Notify.success('ok');
+
+const refs = {
+  searchForm: document.getElementById('search-form'),
+};
+
+refs.searchForm.addEventListener('submit', onSearch);
+
+function onSearch(e) {
+  e.preventDefault();
+
+  const form = e.currentTarget;
+  const searchValue = form.elements.searchQuery.value;
+  getPicture(searchValue);
+
+  //   console.dir(searchValue);
+}
+
+async function getPicture(searchValue) {
+  try {
+    const BASE_URL = 'https://pixabay.com/api/';
+    const API_KEY = 'key=26823171-490067530a76e346906bfc05d';
+
+    const response = await axios.get(
+      `${BASE_URL}?${API_KEY}&q=${searchValue}&image_type=photo&orientation=horizontal&safesearch=true`,
+    );
+    console.log(response.data.hits);
+    return response.data.hits;
+    // console.log(response);
+  } catch (error) {
+    console.error(error);
+  }
+}
+
+// async function renderData(arr) {
+//   console.log(arr.data.hits);
+// }
+
+// renderData(getPicture);
+
+// const options = {
+//       image_type: photo,
+//       orientation: horizontal,
+//       safesearch: true,
+//     };
